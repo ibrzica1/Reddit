75176c0d030a4ffc3578a4d925572e6537acc299
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-09-29 17:04:34 +0100

    Create signup form

---
+++ view/signup.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up - Reddit Style</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="../style/signup.css">
+    
+</head>
+<body>
+    <div class="reddit-container">
+        <form class="reddit-form" method="POST" action="decisionMaker.php">
+            <input type="hidden" name="signup">
+            
+            <div class="form-header">
+                <h2>Sign Up</h2>
+                <p>By continuing, you are setting up a Reddit account and agree to our <a href="#">User Agreement</a> and <a href="#">Privacy Policy</a>.</p>
+            </div>
+
+            <div class="input-group">
+                <input type="text" name="username" placeholder="Username" required>
+            </div>
+            
+            <div class="input-group">
+                <input type="password" name="password" placeholder="Password" required>
+            </div>
+            
+            <div class="input-group">
+                <input type="password" name="password_confirm" placeholder="Confirm Password" required>
+            </div>
+            
+            <button type="submit" class="reddit-btn-primary">Continue</button>
+            
+            <div class="form-footer">
+                <p>Already a Redditor?
