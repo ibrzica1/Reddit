eb3183efe40fe0a3ac0a94dd941ae0f4248b9e0e
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-10-03 09:40:48 +0100

    Create and style header for index

--- index.php
+++ index.php
@@ -13,23 +13,31 @@ require_once "vendor/autoload.php";
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Index</title>
+  <link rel="stylesheet" href="style/header.css">
 </head>
 
 <body>
   
   <div class="header-container">
-    <div class="buttons-container">
-      <div class="login-container">
-      <a href="view/login.php">Login</a>
+    <div class="logo-container">
+        <img src="images/reddit.png" alt="Reddit Logo" class="reddit-logo">
     </div>
-    <div class="signup-container">
-      <a href="view/signup.php">Signup</a>
+    
+    <div class="search-container">
+        <img src="images/icons/magnifying-glass.png" alt="Search Icon" class="search-icon">
+        <input type="text" placeholder="Search Reddit">
+    </div>
+    
+    <div class="buttons-container">
+        <div class="login-container">
+            <a href="view/login.php">Log In</a>
+        </div>
+        <div class="signup-container">
+            <a href="view/signup.php">Sign Up</a>
+        </div>
     </div>
   </div>
     
-  </div>
-
-
 </body>
 
 </html>
