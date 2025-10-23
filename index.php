7eddb6a36360a239a9652f7c4d31f30874ffb15f
ibrzica1 <igorbrzica1@gmail.com>
2025-10-23 17:20:04 +0200

    Finish CSS for the header

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ use Reddit\services\SessionService;
 $session = new SessionService();
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,15 +15,16 @@ $session = new SessionService();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Index</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-  <link rel="stylesheet" href="style/header.css">
+  <link rel="stylesheet" href="style/header.css?v=<?php echo time(); ?>">
+  
 </head>
 
 <body>
   
   <div class="header-container">
-    <div class="logo-container">
+    <a class="logo-container" href="index.php">
         <img src="images/reddit.png" alt="Reddit Logo" class="reddit-logo">
-    </div>
+    </a>
     
     <div class="search-container">
         <img src="images/icons/magnifying-glass.png" alt="Search Icon" class="search-icon">
@@ -37,11 +37,33 @@ $session = new SessionService();
                 <img class='plus-icon' src="images/icons/plus.png">
                 <p>Create</p>
             </a>
-            <div class="user-dropdown">
-                <div class="user-info">
+            <a class="notifications-container" href="">
+                <img src="images/icons/bell.png">
+            </a>
+                <div class="user-info" id="userInfo">
+                    <div class="green-dot"></div>
                     <img class="user-avatar" src="images/avatars/avatarBlue.webp">
+                    
                 </div>
-            </div>
+                <div class="menu-container" id="userMenu">
+                        <div class="profile-container">
+                            <div class="avatar-container">
+                                <img class="user-avatar" src="images/avatars/avatarBlue.webp">
+                            </div>
+                            <div class="info-container">
+                                <h3>View Profile</h3>
+                                <p>u/<?= $session->getFromSession("username") ?></p>
+                            </div>
+                        </div>
+                        <div class="edit-container">
+                            <img src="images/icons/shirt.png">
+                            <p>Edit Avatar</p>
+                        </div>
+                        <div class="logout-container">
+                            <img src="images/icons/house-door.png">
+                            <p>Log Out</p>
+                        </div>
+                    </div>
         </div>
     <?php else: ?>
     <div class="buttons-container">
@@ -55,6 +77,11 @@ $session = new SessionService();
     <?php endif; ?>
   </div>
     
+  <script type="module">
+    import { toggleMenu } from "./script/tools.js";
+    const  menu = document.getElementById("userInfo");
+    menu.addEventListener('click',toggleMenu);
+  </script>
 </body>
 
 </html>
