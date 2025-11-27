e9bb73f2f47b352b9bca290f6679cbf57d10e63e
ibrzica1 <igorbrzica1@gmail.com>
2025-11-27 08:38:58 +0100

    Create function addLikePost

--- src/models/Like.php
+++ src/models/Like.php
@@ -8,9 +8,21 @@ class Like extends Db
     protected $userId;
     public $postId;
     public $commentId;
+    public $status;
 
     public function __construct($idUser)
     {
         $this->userId = $idUser;
+    }
+
+    public function addLikePost($postId,$status)
+    {
+        $stmt = $this->connection->prepare("INSERT INTO like (user_id,post_id,status)
+        VALUES :user_id, :post_id, :status");
+        $stmt->bindParam(':user_id', $this->userId);
+        $stmt->bindParam(':post_id', $postId);
+        $stmt->bindParam(':status', $status);
+
+        $stmt->execute();
     }
 }
