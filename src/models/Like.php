3afecfd4347d588a1f2a45fdcbcd23cde754d614
ibrzica1 <igorbrzica1@gmail.com>
2025-11-28 08:09:52 +0100

    Create function addLikePost and updateLikePost

--- src/models/Like.php
+++ src/models/Like.php
@@ -5,32 +5,46 @@ use Reddit\models\Db;
 
 class Like extends Db
 {
-    protected $userId;
+    public $userId;
     public $postId;
     public $commentId;
     public $status;
 
-    public function __construct($idUser)
-    {
-        $this->userId = $idUser;
-    }
 
-    public function getLike($attribute,$value)
+    public function getLike($attribute,$value,$userId)
     {
-        $stmt = $this->connection->prepare("SELECT * FROM like WHERE $attribute = :value");
+        $stmt = $this->connection->prepare("SELECT * FROM likes WHERE $attribute = :value
+        AND user_id = :user_id");
         $stmt->bindParam(':value',$value);
-        
+        $stmt->bindParam(':user_id',$userId);
         $stmt->execute();
+
+        return $stmt->fetch();
     }
 
-    public function addLikePost($postId,$status)
+    public function addLikePost($postId,$status,$userId)
     {
         $stmt = $this->connection->prepare("INSERT INTO like (user_id,post_id,status)
         VALUES :user_id, :post_id, :status");
-        $stmt->bindParam(':user_id', $this->userId);
+        $stmt->bindParam(':user_id', $userId);
         $stmt->bindParam(':post_id', $postId);
         $stmt->bindParam(':status', $status);
 
         $stmt->execute();
     }
+
+    public function updateLikePost($postId,$status,$userId)
+    {
+        $stmt = $this->connection->prepare("UPDATE likes 
+        SET status = :status
+        WHERE user_id = :user_id
+        AND post_id = :post_id)");
+        $stmt->bindParam(':user_id', $userId);
+        $stmt->bindParam(':post_id', $postId);
+        $stmt->bindParam(':status', $status);
+
+        $stmt->execute();
+    }
+
+    
 }
