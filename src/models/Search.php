3fb790f4b67071da7cb0a4c3ae6bcd29cea65216
ibrzica1 <igorbrzica1@gmail.com>
2025-12-19 17:03:42 +0100

    Create Search class

---
+++ src/models/Search.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Reddit\models;
+use Reddit\models\Db;
+
+class Search extends Db 
+{
+    public function SearchAll($search)
+    {
+        $query = "
+        SELECT
+            'community' AS type,
+            id,
+            name AS display_name,
+            description AS info
+        FROM community
+        WHERE name LIKE :search1
+        UNION ALL
+        SELECT
+            'post' AS type,
+            id,
+            title AS display_name,
+            SUBSTRING(text,1,50) AS info 
+        FROM post 
+        WHERE title LIKE :search2 OR text LIKE :search2
+        UNION ALL
+        SELECT
+            'user' AS type,
+            id,
+            username AS display_name,
+            CONCAT('u/', username) AS info
+        FROM user
+        WHERE username LIKE :search3 
+        LIMIT 10";
+        
+        $stmt = $this->connection->prepare($query);
+        $searchTerm = "%$search%";
+        $stmt->bindValue(':search1',$searchTerm);
+        $stmt->bindValue(':search2',$searchTerm);
+        $stmt->bindValue(':search3',$searchTerm);
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+}
